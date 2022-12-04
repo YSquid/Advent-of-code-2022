1d9527a6a3db11ffbf6b927a13646219b8a5859a
Ysquid <ahk648@gmail.com>
2022-12-04 14:33:51 -0600

    day 4 both parts done

---
+++ Day_4/day4-2.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const pairs = fs.readFileSync("./pairs.txt").toString().split("\n");
+const pairsSplit = pairs.map((pair) => pair.split(","));
+
+
+const ranged = pairsSplit.map((pair) => {
+  return pair.map((range) => {
+    return range.split("-");
+  });
+});
+
+const rangedOut = ranged.map((pair) => {
+  return pair.map((range) => {
+    const min = parseInt(range[0]);
+    const max = parseInt(range[1]);
+    const newRange = [];
+    for (let i = min; i <= max; i++) {
+      newRange.push(i);
+    }
+    return newRange;
+  });
+});
+
+console.log(rangedOut)
+
+const checkRanges = rangedOut.map((pair) => {
+    let arr = pair[0]
+    let target = pair [1]
+    if (pair[0].length < pair[1].length) {
+        arr = pair[1]
+        target = pair[0]
+    }
+
+    let checker = (arr, target) => target.some(v => arr.includes(v));
+
+    if (checker(arr, target)) {
+        return 1
+    } else {
+        return 0
+    }
+})
+
+const sumMatches = checkRanges.reduce((acc, next) => acc + next)
+console.log(checkRanges)
+console.log(sumMatches)
